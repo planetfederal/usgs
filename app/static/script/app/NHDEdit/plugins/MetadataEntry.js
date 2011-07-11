285c10277bb119668d0d981f22e0129a40c062d5
Bart van den Eijnden <bartvde@opengeo.org>
2011-07-11 19:57:25 +0200

    move stuff from plugin to metadata form

--- app/static/script/app/NHDEdit/plugins/MetadataEntry.js
+++ app/static/script/app/NHDEdit/plugins/MetadataEntry.js
@@ -28,114 +28,16 @@ NHDEdit.plugins.MetadataEntry = Ext.extend(gxp.plugins.Tool, {
     /** api: ptype = app_metadataentry */
     ptype: "app_metadataentry",
 
-    featureType: null,
-
-    featureNS: null,
-
-    url: null,
-
-    featureStore: null,
-
-    openWindow: null,
-
-    /** api: property[schema]
-     *  ``GeoExt.data.AttributeStore`` 
-     */
-    schema: null,
-
-    /** private: method[init]
-     */
-    init: function(target) {
-        NHDEdit.plugins.MetadataEntry.superclass.init.apply(this, arguments);
-        this.schema = new GeoExt.data.AttributeStore({
-            url: this.url,
-            baseParams: {
-                SERVICE: "WFS",
-                VERSION: "1.1.0",
-                REQUEST: "DescribeFeatureType",
-                TYPENAME: this.featurePrefix + ":" + this.featureType
-            },
-            autoLoad: true,
-            listeners: {
-                "load": this.createStore,
-                scope: this
-            }
-        });
-    },
-
-    createStore: function() {
-        var fields = [];
-        this.schema.each(function(r) {
-            fields.push({name: r.get("name")});
-        }, this);
-        this.featureStore = new gxp.data.WFSFeatureStore({
-            fields: fields,
-            autoLoad: true,
-            listeners: {
-                "load": this.createGrid,
-                scope: this
-            },
-            url: this.schema.url,
-            featureType: this.featureType,
-            featureNS: this.featureNS
-        });
-    },
-
-    createGrid: function() {
-        Ext.getCmp("app-open-button").setDisabled(false);
-        this.grid = new gxp.grid.FeatureGrid({
-            store: this.featureStore,
-            schema: this.schema,
-            loadMask: true,
-            /* TODO: in this case it would be easier to specify the includeFields */
-            ignoreFields: ['meta_processid', 'attributeaccuracyreport', 'logicalconsistencyreport', 'completenessreport', 'horizpositionalaccuracyreport', 'vertpositionalaccuracyreport', 'metadatastandardname', 'metadatastandardversion', 'metadatadate', 'datasetcredit', 'contactorganization', 'addresstype', 'address', 'city', 'stateorprovince', 'postalcode', 'contactvoicetelephone', 'contactinstructions'],
-            height: 300,
-            bbar: ["->", {text: "Open", iconCls: "gxp-icon-open", handler: this.openMetadata, scope: this}]
-        });
-    },
-
-    saveEntry: function() {
-        var feature = new OpenLayers.Feature.Vector(null, {});
-        this.form.getForm().items.each(function(item) {
-            var value = item.getRawValue();
-            if (value !== "") {
-                feature.attributes[item.name] = value;
-            }
-        });
-        feature.state = OpenLayers.State.INSERT;
-        var options = {
-            callback: function(response) {
-                if (response && response.insertIds) {
-                    this.target.metadataId = response.insertIds[0];
-                    this.removeOutput();
-                }
-            },
-            scope: this
-        };
-        this.featureStore.proxy.protocol.commit([feature], options);
-    },
-
-    openEntry: function() {
-        if (this.openWindow === null) {
-            this.openWindow = new Ext.Window({title: "Open", layout: "fit", items: [this.grid]});
-        }
-        this.openWindow.show();
-    },
-
-    openMetadata: function() {
-        var fid = this.grid.getSelectionModel().getSelected().get("feature").fid;
-        this.target.metadataId = fid;
-        this.openWindow.close();
-        this.removeOutput();
-    },
-
     /** api: method[addOutput]
      */
     addOutput: function() {
         this.form = new NHDEdit.MetadataForm({
             labelWidth: 200,
-            autoHeight: true,
-            fbar: [
+            url: this.url,
+            featureType: this.featureType,
+            featureNS: this.featureNS, 
+            autoHeight: true
+/*            fbar: [
                 {
                     text: "Open",
                     iconCls: "gxp-icon-open",
@@ -149,15 +51,7 @@ NHDEdit.plugins.MetadataEntry = Ext.extend(gxp.plugins.Tool, {
                     handler: this.saveEntry, 
                     scope: this
                 }
-            ],
-            schema: this.schema    
-        });
-        this.grid = new gxp.grid.FeatureGrid({
-            loadMask: true,
-            /* TODO: in this case it would be easier to specify the includeFields */
-            ignoreFields: ['meta_processid', 'attributeaccuracyreport', 'logicalconsistencyreport', 'completenessreport', 'horizpositionalaccuracyreport', 'vertpositionalaccuracyreport', 'metadatastandardname', 'metadatastandardversion', 'metadatadate', 'datasetcredit', 'contactorganization', 'addresstype', 'address', 'city', 'stateorprovince', 'postalcode', 'contactvoicetelephone', 'contactinstructions'],
-            height: 300,
-            bbar: ["->", {text: "Open", iconCls: "gxp-icon-open", handler: this.openMetadata, scope: this}]
+            ],*/
         });
         return NHDEdit.plugins.MetadataEntry.superclass.addOutput.call(this, this.form);
     },
--- app/static/script/app/NHDEdit/widgets/MetadataForm.js
+++ app/static/script/app/NHDEdit/widgets/MetadataForm.js
@@ -21,21 +21,122 @@ Ext.ns("NHDEdit");
  */
 NHDEdit.MetadataForm = Ext.extend(Ext.form.FormPanel, {
 
+    url: null,
+
     schema: null,
 
-    border: false,
+    featureType: null,
 
-    bodyStyle: "padding: 5px",
+    featureNS: null,
+
+    featureStore: null,
+
+    openWindow: null,
 
     initComponent : function() {
+        this.fbar = [
+            {
+                text: "Open",
+                iconCls: "gxp-icon-open",
+                disabled: true,
+                id: "app-open-button",
+                handler: this.openEntry,
+                scope: this
+            }, {
+                text: "Save",
+                iconCls: "gxp-icon-save",
+                handler: this.saveEntry,
+                scope: this,
+                id: "app-save-button",
+                disabled: true
+            }
+        ];
+        this.schema = new GeoExt.data.AttributeStore({
+            url: this.url,
+            baseParams: {
+                SERVICE: "WFS",
+                VERSION: "1.1.0",
+                REQUEST: "DescribeFeatureType",
+                TYPENAME: this.featurePrefix + ":" + this.featureType
+            },
+            autoLoad: true,
+            listeners: {
+                "load": this.onLoad,
+                scope: this
+            }
+        });
         NHDEdit.MetadataForm.superclass.initComponent.call(this);
-        this.schema.on({
-            "load": this.onLoad,
+    },
+
+    createGrid: function() {
+        Ext.getCmp("app-open-button").setDisabled(false);
+        this.grid = new gxp.grid.FeatureGrid({
+            store: this.featureStore,
+            schema: this.schema,
+            loadMask: true,
+            /* TODO: in this case it would be easier to specify the includeFields */
+            ignoreFields: ['meta_processid', 'attributeaccuracyreport', 'logicalconsistencyreport', 'completenessreport', 'horizpositionalaccuracyreport', 'vertpositionalaccuracyreport', 'metadatastandardname', 'metadatastandardversion', 'metadatadate', 'datasetcredit', 'contactorganization', 'addresstype', 'address', 'city', 'stateorprovince', 'postalcode', 'contactvoicetelephone', 'contactinstructions'],
+            height: 300,
+            bbar: ["->", {text: "Open", iconCls: "gxp-icon-open", handler: this.openMetadata, scope: this}]
+        });
+    },
+
+    saveEntry: function() {
+        var feature = new OpenLayers.Feature.Vector(null, {});
+        this.getForm().items.each(function(item) {
+            var value = item.getRawValue();
+            if (value !== "") {
+                feature.attributes[item.name] = value;
+            }
+        });
+        feature.state = OpenLayers.State.INSERT;
+        var options = {
+            callback: function(response) {
+                if (response && response.insertIds) {
+                    // TODO: figure out how to give metadataId to viewer
+                    //this.target.metadataId = response.insertIds[0];
+                }
+            },
             scope: this
+        };
+        this.featureStore.proxy.protocol.commit([feature], options);
+    },
+
+    openEntry: function() {
+        if (this.openWindow === null) {
+            this.openWindow = new Ext.Window({title: "Open", layout: "fit", items: [this.grid]});
+        }
+        this.openWindow.show();
+    },
+
+    openMetadata: function() {
+        var fid = this.grid.getSelectionModel().getSelected().get("feature").fid;
+        // TODO: figure out how to give metadataId to viewer
+        //this.target.metadataId = fid;
+        this.openWindow.close();
+    },
+
+    createStore: function() {
+        var fields = [];
+        this.schema.each(function(r) {
+            fields.push({name: r.get("name")});
+        }, this);
+        this.featureStore = new gxp.data.WFSFeatureStore({
+            fields: fields,
+            autoLoad: true,
+            listeners: {
+                "load": this.createGrid,
+                scope: this            
+            },
+            url: this.schema.url,
+            featureType: this.featureType,
+            featureNS: this.featureNS
         });
     },
 
     onLoad: function() {
+        this.createStore();
+        Ext.getCmp("app-save-button").setDisabled(false);
         var commitFieldset = new Ext.form.FieldSet({title: "Commit message"});
         var fieldSet = new Ext.form.FieldSet({collapsible: true, collapsed: true, title: "Advanced"});
         this.schema.each(function(r) {
