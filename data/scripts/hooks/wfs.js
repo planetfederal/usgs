7d18ed51e7cf73462ae279a3cba02bc0a468ed56
tschaub <tim.schaub@gmail.com>
2011-07-19 20:38:53 -0600

    Defer rule running when native includes queue true.

--- data/scripts/hooks/wfs.js
+++ data/scripts/hooks/wfs.js
@@ -12,35 +12,39 @@ exports.beforeCommit = function(details, request) {
         };
     }
     var inserts = details.PreInsert || [];
-    var featureInfo, feature, rules, rule, filter, process, outputs;
+    var featureInfo, feature, rules, rule, inputs, filter, process, outputs;
+
     for (var i=0, ii=inserts.length; i<ii; ++i) {
         featureInfo = inserts[i];
         feature = featureInfo.feature;
         rules = usgs.getRules(featureInfo);
         for (var j=0, jj=rules.length; j<jj; ++j) {
             rule = rules[i];
-            if (rule.objectFTypes) {
-                filter = where.apply(null, ["FType IN"].concat(rule.objectFTypes));
-            } else {
-                filter = undefined;
-            }
-            process = require("processes/" + rule.name).process;
-            outputs = process.run({
-                geometry: feature.geometry,
-                featureType: rule.objectLayer,
-                namespace: usgs.NAMESPACE_URI,
-                filter: filter
-            });
-            if (outputs.result == false) {
-                // TODO: native handling
-                return {
-                    locator: "js:" + rule.name,
-                    // TODO: determine where message is composed
-                    message: JSON.stringify({
-                        message: process.title + " Failed",
-                        rule: rule
-                    })
-                };
+            inputs = nativ["js:" + rule.name] || {};
+            if (!inputs.queue) {
+                if (rule.objectFTypes) {
+                    filter = where.apply(null, ["FType IN"].concat(rule.objectFTypes));
+                } else {
+                    filter = undefined;
+                }
+                process = require("processes/" + rule.name).process;
+                outputs = process.run({
+                    geometry: feature.geometry,
+                    featureType: rule.objectLayer,
+                    namespace: usgs.NAMESPACE_URI,
+                    filter: filter
+                });
+                if (outputs.result == false) {
+                    // TODO: native handling
+                    return {
+                        locator: "js:" + rule.name,
+                        // TODO: determine where message is composed
+                        message: JSON.stringify({
+                            message: process.title + " Failed",
+                            rule: rule
+                        })
+                    };
+                }
             }
         }
     }
@@ -51,7 +55,7 @@ exports.beforeCommit = function(details, request) {
  * is assumed to be a JSON string.
  */
 function parseNatives(natives) {
-    var nativ, obj;
+    var nativ, obj = {};
     for (var i=0, ii=natives.length; i<ii; ++i) {
         nativ = natives[i];
         if (nativ.vendorId === "usgs") {
