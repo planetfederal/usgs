1237096ae37c4f6b67cf941081982f529619f17b
ahocevar <andreas.hocevar@gmail.com>
2011-07-22 13:31:38 +0200

    fixing a typo that resulted in store events not being unregistered (closes #51)

--- app/static/script/app/NHDEdit/widgets/FeatureEditWizard.js
+++ app/static/script/app/NHDEdit/widgets/FeatureEditWizard.js
@@ -271,7 +271,7 @@ NHDEdit.FeatureEditWizard = Ext.extend(Ext.Window, {
             },
             "beforeclose": this.handleBeforeClose,
             "beforedestroy": function() {
-                this.store.un("exception", this.handleException, this);
+                this.store.un("exception", this.handleStoreException, this);
                 this.store.commitChanges();
                 this.editing = false;
             },
