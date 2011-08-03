8e57f9caa80b338825f50c1c438e48633604940c
ahocevar <andreas.hocevar@gmail.com>
2011-08-03 18:17:51 +0200

    exception templates are now globally accessible.

---
+++ app/static/script/app/NHDEdit/templates.js
@@ -0,0 +1,47 @@
+Ext.ns("NHDEdit");
+NHDEdit.exceptionTemplates = {
+    "js:MustIntersect": new Ext.XTemplate(
+        ['{subjectFType:this.getFType} {subjectLayer} features must ', 
+        'intersect a feature from one of the following layers: ',
+        '<tpl for="objects">{layer}<tpl if="values.ftypes"> (',
+        '<tpl for="ftypes">{.:this.getFType}{[xindex < xcount ? ", " : ""]}</tpl>)</tpl>',
+        '{[xindex < xcount ? ", " : ""]}</tpl>.<tpl if="values.autoCorrectable">',
+        ' This exception can be autocorrected.</tpl>'].join(""),
+        {
+            getFType: function(value) {
+                return NHDEdit.fTypeDict[value];
+            }
+        }
+    ),
+    "js:MustIntersectEndpoint": new Ext.XTemplate(
+        ['{subjectFType:this.getFType} {subjectLayer} features must ', 
+        'intersect an endpoint (the first or last point) of a feature ',
+        'from one of the following layers: ',
+        '<tpl for="objects">{layer}<tpl if="values.ftypes"> (',
+        '<tpl for="ftypes">{.:this.getFType}{[xindex < xcount ? ", " : ""]}</tpl>)</tpl>',
+        '{[xindex < xcount ? ", " : ""]}</tpl>.<tpl if="values.autoCorrectable">',
+        ' This exception can be autocorrected.</tpl>'].join(""),
+        {
+            getFType: function(value) {
+                return NHDEdit.fTypeDict[value];
+            }
+        }
+    ),
+    "js:MustNotCross": new Ext.XTemplate(
+        ['{subjectFType:this.getFType} {subjectLayer} features must not cross ',
+        'any features from any of the following layers: ',
+        '<tpl for="objects">{layer}<tpl if="values.ftypes"> (',
+        '<tpl for="ftypes">{.:this.getFType}{[xindex < xcount ? ", " : ""]}</tpl>)</tpl>',
+        '{[xindex < xcount ? ", " : ""]}</tpl>.<tpl if="values.autoCorrectable">',
+        ' This exception can be autocorrected.</tpl>'].join(""),
+        {
+            getFType: function(value) {
+                return NHDEdit.fTypeDict[value];
+            }
+        }
+    ),
+    "js:MustHaveVerticalRelationship": new Ext.XTemplate(
+        ['Pipeline {layer} features must have a vertical relationship. ',
+        '<tpl if="values.autoCorrectable">This exception can be autocorrected.</tpl>'].join("")
+    )
+};
--- app/static/script/app/NHDEdit/widgets/ExceptionPanel.js
+++ app/static/script/app/NHDEdit/widgets/ExceptionPanel.js
@@ -39,53 +39,6 @@ NHDEdit.ExceptionPanel = Ext.extend(Ext.form.FormPanel, {
      */
     autoCorrectValue: null,
     
-    templates: {
-        "js:MustIntersect": new Ext.XTemplate(
-            ['{subjectFType:this.getFType} {subjectLayer} features must ', 
-            'intersect a feature from one of the following layers: ',
-            '<tpl for="objects">{layer}<tpl if="values.ftypes"> (',
-            '<tpl for="ftypes">{.:this.getFType}{[xindex < xcount ? ", " : ""]}</tpl>)</tpl>',
-            '{[xindex < xcount ? ", " : ""]}</tpl>.<tpl if="values.autoCorrectable">',
-            ' This exception can be autocorrected.</tpl>'].join(""),
-            {
-                getFType: function(value) {
-                    return NHDEdit.fTypeDict[value];
-                }
-            }
-        ),
-        "js:MustIntersectEndpoint": new Ext.XTemplate(
-            ['{subjectFType:this.getFType} {subjectLayer} features must ', 
-            'intersect an endpoint (the first or last point) of a feature ',
-            'from one of the following layers: ',
-            '<tpl for="objects">{layer}<tpl if="values.ftypes"> (',
-            '<tpl for="ftypes">{.:this.getFType}{[xindex < xcount ? ", " : ""]}</tpl>)</tpl>',
-            '{[xindex < xcount ? ", " : ""]}</tpl>.<tpl if="values.autoCorrectable">',
-            ' This exception can be autocorrected.</tpl>'].join(""),
-            {
-                getFType: function(value) {
-                    return NHDEdit.fTypeDict[value];
-                }
-            }
-        ),
-        "js:MustNotCross": new Ext.XTemplate(
-            ['{subjectFType:this.getFType} {subjectLayer} features must not cross ',
-            'any features from any of the following layers: ',
-            '<tpl for="objects">{layer}<tpl if="values.ftypes"> (',
-            '<tpl for="ftypes">{.:this.getFType}{[xindex < xcount ? ", " : ""]}</tpl>)</tpl>',
-            '{[xindex < xcount ? ", " : ""]}</tpl>.<tpl if="values.autoCorrectable">',
-            ' This exception can be autocorrected.</tpl>'].join(""),
-            {
-                getFType: function(value) {
-                    return NHDEdit.fTypeDict[value];
-                }
-            }
-        ),
-        "js:MustHaveVerticalRelationship": new Ext.XTemplate(
-            ['Pipeline {layer} features must have a vertical relationship. ',
-            '<tpl if="values.autoCorrectable">This exception can be autocorrected.</tpl>'].join("")
-        )
-    },
-
     ruleSpecificItems: {
         // specific handling for vertical relationship rule
         "6": function() {
@@ -136,7 +89,7 @@ NHDEdit.ExceptionPanel = Ext.extend(Ext.form.FormPanel, {
         var known = false,
             code = this.getProperty("code"),
             locator = this.getProperty("locator"),
-            tpl = this.templates[locator];
+            tpl = NHDEdit.exceptionTemplates[locator];
         if (tpl) {
             // this is a known exception type, we expect a parseable message
             var messageObj = this.getMessageObject();
