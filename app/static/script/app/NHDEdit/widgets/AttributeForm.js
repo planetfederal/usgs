7041ad433ab5efb3c59d62b43728a5760872ca7a
tschaub <tim.schaub@gmail.com>
2011-08-04 16:17:43 -0600

    Displaying FType and FCode fields first (closes #85).

--- app/static/script/app/NHDEdit/widgets/AttributeForm.js
+++ app/static/script/app/NHDEdit/widgets/AttributeForm.js
@@ -46,10 +46,62 @@ NHDEdit.AttributeForm = Ext.extend(Ext.form.FormPanel, {
             data : NHDEdit.getFCodes(typeName)
         });
         fCodeStore.sort("value");
+        
         var fieldset = new Ext.form.FieldSet({title: "Attributes", defaults: {width: 150}});
+
+        // ftype field first
+        fieldset.add({
+            xtype: "combo",
+            mode: "local",
+            forceSelection: true,
+            allowBlank: false,
+            name: "FType",
+            fieldLabel: "FType",
+            value: this.feature.attributes.FType,
+            store: fTypeStore, 
+            triggerAction: "all",
+            displayField: "description",
+            valueField: "value",
+            listeners: {
+                valid: function(field) {
+                    window.setTimeout(function() {
+                        var value = field.getValue();
+                        fCodeStore.filterBy(function(r) {
+                            return r.get("value").indexOf(value) === 0;
+                        }, this);
+                        var fCode = field.ownerCt.fCode;
+                        if (fCodeStore.findExact("value", fCode.getValue()) == -1) {
+                            fCode.setValue(fCodeStore.getAt(0).get("value"));
+                        }
+                    }, 0);
+                }
+            }
+        });
+        
+        // fcode field next
+        fieldset.add({
+            xtype: "combo",
+            ref: "fCode",
+            allowBlank: false,
+            listWidth: 650,
+            mode: "local",
+            lastQuery: "",
+            forceSelection: true,
+            name: "FCode",
+            fieldLabel: "FCode",
+            value: this.feature.attributes.FCode,
+            store: fCodeStore, 
+            triggerAction: "all",
+            displayField: "description",
+            valueField: "value"
+        });
+        
+        // all remaining fields
         this.schema.each(function(r) {
             var name = r.get("name");
-            if (this.excludeFields.indexOf(name) != -1) {
+            if (this.excludeFields.indexOf(name) != -1 || 
+                    name.toLowerCase() == "ftype" || 
+                    name.toLowerCase() == "fcode") {
                 return;
             }
             var type = r.get("type");
@@ -57,53 +109,7 @@ NHDEdit.AttributeForm = Ext.extend(Ext.form.FormPanel, {
                 // exclude gml geometries
                 return;
             }
-            var fieldCfg;
-            if (name.toLowerCase() == "ftype") {
-                fieldCfg = {
-                    xtype: "combo",
-                    mode: "local",
-                    forceSelection: true,
-                    allowBlank:false,
-                    name: name,
-                    fieldLabel: name,
-                    store: fTypeStore, 
-                    triggerAction: 'all',
-                    displayField: 'description',
-                    valueField: 'value',
-                    listeners: {
-                        "valid": function(field) {
-                            window.setTimeout(function() {
-                                var value = field.getValue();
-                                fCodeStore.filterBy(function(r) {
-                                    return r.get("value").indexOf(value) === 0;
-                                }, this);
-                                var fCode = field.ownerCt.fCode;
-                                if (fCodeStore.findExact("value", fCode.getValue()) == -1) {
-                                    fCode.setValue(fCodeStore.getAt(0).get("value"));
-                                }
-                            }, 0);
-                        } 
-                    }
-                };
-            } else if (name.toLowerCase() == "fcode") {
-                fieldCfg = {
-                    xtype: "combo",
-                    ref: "fCode",
-                    allowBlank:false,
-                    listWidth: 650,
-                    mode: "local",
-                    lastQuery: "",
-                    forceSelection: true,
-                    name: name,
-                    fieldLabel: name,
-                    store: fCodeStore, 
-                    triggerAction: 'all',
-                    displayField: 'description',
-                    valueField: 'value'
-                };            
-            } else {
-                fieldCfg = GeoExt.form.recordToField(r);
-            }
+            var fieldCfg = GeoExt.form.recordToField(r);
             fieldCfg.value = this.feature.attributes[name];
             if (fieldCfg.value && fieldCfg.xtype == "datefield") {
                 var dateFormat = "Y-m-d";
@@ -112,6 +118,7 @@ NHDEdit.AttributeForm = Ext.extend(Ext.form.FormPanel, {
             }
             fieldset.add(fieldCfg);
         }, this);
+
         this.add(fieldset);
     }
 
