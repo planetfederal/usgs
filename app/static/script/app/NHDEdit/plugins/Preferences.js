124d1b63be03d5d9e84b69fdb02d0a894fe7eedc
Bart van den Eijnden <bartvde@opengeo.org>
2011-09-02 11:39:38 +0200

    add api doc for Prefences, also do some cleanup in destroy

--- app/static/script/app/NHDEdit/plugins/Preferences.js
+++ app/static/script/app/NHDEdit/plugins/Preferences.js
@@ -17,7 +17,9 @@ Ext.ns("NHDEdit.plugins");
 /** api: constructor
  *  .. class:: Preferences(config)
  *
- *    Preferences dialog.
+ *  Preferences dialog. The prefences dialog allows the end user to set
+ *  preferences such as "always use over for the vertical relationship" 
+ *  question. The preferences are tied to a specific metadata record.  
  */
 NHDEdit.plugins.Preferences = Ext.extend(gxp.plugins.Tool, {
 
@@ -27,11 +29,24 @@ NHDEdit.plugins.Preferences = Ext.extend(gxp.plugins.Tool, {
     /** i18n */
     buttonText: "Preferences",
     tooltipText: "Change editing preferences",
+    /** end i18n */
 
+    /** private: config[vendorId]
+     *  ``String``
+     *  The vendorId attribute value of the Native element of a WFS 
+     *  Transaction.
+     */
     vendorId: "usgs",
+
+    /** api: config[featureManager]
+     * The feature manager to use for this tool. This is needed to hook up
+     * to the layerchange event of the feature manager.
+     */
     featureManager: null,
 
     /** private: method[init]
+     *
+     *  :arg target: ``gxp.Viewer`` The viewer initializing this plugin.
      */
     init: function(target) {
         NHDEdit.plugins.Preferences.superclass.init.apply(this, arguments);
@@ -40,6 +55,15 @@ NHDEdit.plugins.Preferences = Ext.extend(gxp.plugins.Tool, {
         target.addListener('setpreference', this.onSetPreference, this);
     },
 
+    /** private: method[onLayerChange]
+     *  If the layer changes that the feature manager is working on, we need
+     *  to register a beforewrite listener on its store, so that we can pass on
+     *  the preferences in the Native element of the WFS Transaction.
+     *
+     *  :arg tool: ``gxp.plugins.FeatureManager`` The feature manager.
+     *  :arg layer: ``GeoExt.data.LayerRecord`` the new layer
+     *  :arg schema: ``GeoExt.data.AttributeStore``
+     */
     onLayerChange: function(tool, layer, schema) {
         var store = tool.featureStore;
         if (store) {
@@ -47,11 +71,27 @@ NHDEdit.plugins.Preferences = Ext.extend(gxp.plugins.Tool, {
         }
     },
 
+    /** private: method[onSetPreference]
+     *  When a prefence has been set (e.g. loaded from an existing metadata 
+     *  record), enable the Preferences button in the UI.
+     *
+     *  :arg code: ``Object`` Rule with an exception code.
+     *  :arg pref: ``Object`` The preferences object associated with the code.
+     */
     onSetPreference: function(code, pref) {
         this.actions[0].enable();
-        // TODO update form if dialog already open
     },
     
+    /**
+     * private: method[beforeStoreWrite]
+     * beforewrite handle of the storer. This handler is used to set up the 
+     * Native element in the WFS Transaction with the preferences information.
+     * 
+     * :arg store: ``Ext.data.Store`` The store
+     * :arg action: ``String`` create, update or destroy
+     * :arg rs: ``Ext.data.Record`` The record set (can also be an array)
+     * :arg options: ``Object`` The loading options that were specified.
+     */
     beforeStoreWrite: function(store, action, rs, options) {
         if (NHDEdit.preferences) {
             var nativeElements = options.params.nativeElements;
@@ -69,6 +109,7 @@ NHDEdit.plugins.Preferences = Ext.extend(gxp.plugins.Tool, {
     },
     
     /** api: method[addOutput]
+     * Creates the output for this tool.
      */
     addOutput: function() {
         var items = [], rule;
@@ -91,18 +132,37 @@ NHDEdit.plugins.Preferences = Ext.extend(gxp.plugins.Tool, {
     },
 
     /** api: method[addActions]
+     * Creates the actions for this tool.
      */
     addActions: function() {
-        return NHDEdit.plugins.Preferences.superclass.addActions.call(this, [
-            {
-                handler: this.addOutput,
-                scope: this,
-                disabled: true,
-                text: this.buttonText,
-                tooltip: this.tooltipText,
-                iconCls: "process"
+        return NHDEdit.plugins.Preferences.superclass.addActions.call(this, [{
+            handler: this.addOutput,
+            scope: this,
+            disabled: true,
+            text: this.buttonText,
+            tooltip: this.tooltipText,
+            iconCls: "process"
+        }]);
+    },
+
+    /** private: method[destroy]
+     * Clean up.
+     */
+    destroy: function() {
+        this.form = null;
+        if (this.target) {
+            if (this.target.tools && this.featureManager) {
+                var featureManager = this.target.tools[this.featureManager];
+                if (featureManager) {
+                    featureManager.removeListener('layerchange', this.onLayerChange, this);
+                    if (featureManager.featureStore) {
+                        featureManager.featureStore.removeListener('beforewrite', this.beforeStoreWrite, this);
+                    }
+                }
             }
-        ]);
+            this.target.removeListener('setpreference', this.onSetPreference, this);
+        }
+        NHDEdit.plugins.Preferences.superclass.destroy.apply(this, arguments);
     }
 
 });
